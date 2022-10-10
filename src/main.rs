a1fbbab5e247ad0f8f2670f06fc1ade9634b6f4d
shimo8810 <shimo.8810@gmail.com>
2022-10-10 15:07:40 +0900

    [update] mainでparserまで実行するように修正

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use the_super_tiny_compiler::tokenizer::tokenizer;
+use the_super_tiny_compiler::{parser::parser, tokenizer::tokenizer};
 
 fn main() {
     let s = "(add 2 (subtract 4 2))";
-    let tokens = tokenizer(s);
+    let tokens = tokenizer(s).unwrap();
     println!("{:?}", tokens);
+    let ast = parser(tokens).unwrap();
+    println!("{:?}", ast);
 }
